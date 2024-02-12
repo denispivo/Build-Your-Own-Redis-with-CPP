f23ec0bcfe2db51c694267d49639ca2cc74829b7
denispivo <69328631+denispivo@users.noreply.github.com>
2024-02-12 04:02:45 +0100

    update 03_client.cpp

--- 03/03_client.cpp
+++ 03/03_client.cpp
@@ -6,7 +6,7 @@
 #include <errno.h>
 //#include <arpa/inet.h>
 #include <sys/socket.h>
-//#include <netinet/ip.h>
+#include <netinet/ip.h>
 
 static void error(const char *msg) {
 
@@ -20,10 +20,14 @@ static void error(const char *msg) {
 int main() {
 
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    fd = -1;
     if (fd < 0) {
         error("socket()");
     }
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = 1234;
+    addr.sin_addr.s_addr = INADDR_LOOPBACK; // 127.0.0.1
 
     return 0;
 
