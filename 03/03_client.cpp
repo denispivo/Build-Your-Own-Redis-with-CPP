e90cb155cc7ec4fbc5f3a71e24334a66016ee115
denispivo <69328631+denispivo@users.noreply.github.com>
2024-02-14 15:43:50 +0100

    finished: client + server
    should be working good, except for the msg the client reseaves. it gets one additional letter for some reason, but at localhost:1234 it is correct

--- 03/03_client.cpp
+++ 03/03_client.cpp
@@ -8,8 +8,6 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 
-#include <cstdlib>
-
 static void error(const char* msg) {
 
     // errno is a preprocessor macro used for error indication
@@ -21,21 +19,35 @@ static void error(const char* msg) {
 
 int main() {
 
+    // create an socket and define fd with the file descriptor of the socket
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
         error("socket()");
     }
 
-    struct sockaddr_in addr = {};
+    // bind the address with the local fd of the socket
+    // create wildcard address 127.0.0.1:1234
+    struct sockaddr_in addr;
     addr.sin_family = AF_INET;
-    addr.sin_port = 1234;
-    addr.sin_addr.s_addr = INADDR_LOOPBACK; // 127.0.0.1
-    int rv = connect(fd, (const struct sockaddr*)&addr, sizeof(addr));
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    //printf("rv = %d\n", rv);
     if (rv) {
-
-        error("connect");
-
+        error("connect()");
     }
+
+    char msg[] = "hello";
+    write(fd, msg, strlen(msg));
+
+    char rbuf[64];
+    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
+    if (n < 0) {
+        error("read()");
+    }
+    printf("server says: %s\n", rbuf);
+
+    close(fd);
 
     return 0;
 
